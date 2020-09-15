4848a6ed171140eac7ff183129ad8578e087f4a2
youneselb <youneselb@gmail.com>
2020-09-15 11:30:38 +0200

    Rest endpoint for /all

--- src/main/java/rest/MembersResource.java
+++ src/main/java/rest/MembersResource.java
@@ -4,14 +4,17 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import utils.EMF_Creator;
 import facades.MembersFacade;
+import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("xxx")
+@Path("Members")
 public class MembersResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
@@ -35,4 +38,14 @@ public class MembersResource {
         //System.out.println("--------------->"+count);
         return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
     }
+    
+    @Path("/all")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getAll() {
+        //throw new UnsupportedOperationException();
+        List<MembersDTO> movies = FACADE.getAllMembers();
+        return new Gson().toJson(movies);
+    }
+  
 }
