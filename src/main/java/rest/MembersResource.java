31371e1dfb6da4471abfe04aefa9a7575881bc6c
Henrik <Henne3660@hotmail.com>
2020-09-15 12:29:46 +0200

    stuck på array object shit

--- src/main/java/rest/MembersResource.java
+++ src/main/java/rest/MembersResource.java
@@ -2,6 +2,7 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dto.MembersDTO;
 import utils.EMF_Creator;
 import facades.MembersFacade;
 import java.util.List;
