4c8d183cc88c6d0adbe39448f58400ac02f0467f
youneselb <youneselb@gmail.com>
2020-09-15 09:09:33 +0200

    MembersDTO

---
+++ src/main/java/dto/MembersDTO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author Yones
+ */
+public class MembersDTO { 
+
+    
+    private int id;
+    private String name;
+    private String studentID;
+    private String movie;
+            
+    public MembersDTO(){}
+    
+    public MembersDTO(Members members) {
+        this.id = members.getid();
+        this.name = members.getname();
+        this.studentID = members.getstudentID();
+        this.movie = members.getmovie();
+        
+          }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+
+
+}
