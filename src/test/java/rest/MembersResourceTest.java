92646ee572db80f979b98f7e40b124dd65a3beca
Henrik <Henne3660@hotmail.com>
2020-09-17 10:12:01 +0200

    merge done

--- src/test/java/rest/MembersResourceTest.java
+++ src/test/java/rest/MembersResourceTest.java
@@ -160,7 +160,4 @@ public class MembersResourceTest {
                 .body("[0].id", equalTo(m1.getId().intValue()));
     }
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 6c9c603054030ce08457cfa91adf53dbe867cce5
